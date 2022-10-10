9060074808eacabaff9cf84a26f173bca216710e
ankitranpara <ankitranpara82@gmail.com>
2022-10-10 22:52:52 +0530

    useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
+import React from 'react';
 import './App.css';
 
 function App() {
+  /**
+   * 
+   * @type {import('react').FormEventHandler<HTMLFormElement>} e 
+   */
+  function displaydata(e){
+    e.preventDefault();
+    console.log(e.target);
+    const formData= new FormData(e.target)
+    const title= formData.get("title")
+    const author= formData.get("author");
+    const headers = new Headers();
+    headers.append('content-type','application/json');
+    fetch('http://localhost:3000/posts',{
+      method:'POST',
+      headers,
+      body: JSON.stringify({title,author})
+    })    
+    .then((res) => res.json())
+    .then((body) => {
+      console.log(body);
+      alert("Post is created");
+    });
+  }
   return (
-    <div className="App">
-    </div>
+    <form onSubmit={displaydata}>
+      <div className="row">
+      <div className="col">
+        <input  className="form-control" type="text" name="title"></input>
+      </div>
+      <div className="col">
+        <input  className="form-control" type="text" name="author"></input>
+      </div>
+      <div className="col">
+        <button className="btn btn-primary col-sm-2" type="submit">Post</button>
+      </div>
+      </div>
+    </form>
   );
 }
 
